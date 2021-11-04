b3de4ea805273c487480648dbd374afc5741bf4a
sidneys9656 <91750545+sidneys9656@users.noreply.github.com>
2021-11-04 12:43:16 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let ownerName = "Sidney Stein"; // update the part BETWEEN the "quotes"
+let userName = "sidneys9656"; // same here
+
+document.querySelectorAll(".owner-name").forEach((e) => {
+    e.innerHTML = ownerName;
+});
+
+document.querySelector("#github").href = "http://github.com/" + userName;
+document.querySelector("#fork").href = "http://github.com/" + userName + "/" + userName + ".github.io";
